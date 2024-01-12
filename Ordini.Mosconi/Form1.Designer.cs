19c66acfa56071cbf4934dd5754083e0b91a780f
Simone Mosconi <simonemosconi05@gmail.com>
2024-01-12 11:36:51 +0100

    Aggiunta aggiona

--- Ordini.Mosconi/Form1.Designer.cs
+++ Ordini.Mosconi/Form1.Designer.cs
@@ -30,13 +30,16 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,34 +47,35 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, -1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(802, 455);
+            this.tabControl1.Size = new System.Drawing.Size(802, 453);
             this.tabControl1.TabIndex = 0;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(794, 429);
+            this.tabPage1.Size = new System.Drawing.Size(794, 427);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Clienti";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // label1
             // 
-            this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(794, 429);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Ordini";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filtro per:";
             // 
             // dataGridView1
             // 
@@ -81,6 +85,17 @@
             this.dataGridView1.Size = new System.Drawing.Size(489, 429);
             this.dataGridView1.TabIndex = 0;
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(794, 427);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ordini";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -89,19 +104,47 @@
             this.dataGridView2.Size = new System.Drawing.Size(489, 429);
             this.dataGridView2.TabIndex = 1;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nessuno",
+            "Id",
+            "Nome",
+            "Cognome",
+            "Email"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 32);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(680, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Aggiorna";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 497);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
@@ -114,6 +157,9 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Ordini.Mosconi/Form1.cs
+++ Ordini.Mosconi/Form1.cs
@@ -20,23 +20,49 @@ namespace Ordini.Mosconi
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            tabControl1.SelectedIndex = 2;
+            comboBox1.SelectedIndex = 0;
+            //comboBox2.SelectedIndex = 0;
+
+            Aggiorna();
+        }
+
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            String ConnectionString = "server=127.0.0.1;uid=Utente1;pwd=password;database=ordini";
+            Aggiorna();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aggiorna();
+        }
+
+        private void Aggiorna()
+        {
+            String ConnectionString = "server=localhost;uid=Utente1;pwd=password;database=ordini";
             MySqlConnection conn = new MySqlConnection(ConnectionString);
             conn.Open();
+            MySqlCommand cmd1;
+            MySqlDataAdapter MyAdapter;
+            DataTable dati = null;
 
-            String sql1 = "select * from clienti;";
-            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
-            cmd1.ExecuteNonQuery();
-            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
-            MyAdapter.SelectCommand = cmd1;
-            DataTable dati = new DataTable();
-            MyAdapter.Fill(dati);
+            if (comboBox1.SelectedIndex == 0)
+            {
+                String sql1 = "select * from clienti;";
+                cmd1 = new MySqlCommand(sql1, conn);
+                cmd1.ExecuteNonQuery();
+                MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd1;
+                dati = new DataTable();
+                MyAdapter.Fill(dati);
+            }
 
 
-
-            String sql2 = "select * from ordini;";
+            String sql2 = "select ordini.id, email, ordini.data_ordine, ordini.importo, ordini.oggetto from clienti join ordini on clienti.id=ordini.cliente_id;";
             cmd1 = new MySqlCommand(sql2, conn);
             cmd1.ExecuteNonQuery();
             MyAdapter = new MySqlDataAdapter();
@@ -44,16 +70,10 @@ namespace Ordini.Mosconi
             DataTable dati2 = new DataTable();
             MyAdapter.Fill(dati2);
 
+
             dataGridView1.DataSource = dati;
             dataGridView2.DataSource = dati2;
             conn.Close();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            tabControl1.SelectedIndex = 2;
         }
     }
 }
